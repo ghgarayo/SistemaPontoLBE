f1e48029509d11e0207c9f8e3ca2076042790937
Gustavo Garayo <gustavo.garayo@kron.digital>
2023-06-12 16:33:16 -0300

    Criacao testes controladoras de autenticacao e registro de ponto

---
+++ back/sistema-ponto/src/test/java/com/lbe/sistemaponto/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,63 @@
+package com.lbe.sistemaponto.controller;
+
+import com.lbe.sistemaponto.domain.endereco.DadosEndereco;
+import com.lbe.sistemaponto.domain.funcionario.DadosCadastroFuncionario;
+import com.lbe.sistemaponto.domain.funcionario.Funcionario;
+import com.lbe.sistemaponto.infra.authentication.DadosAutenticacao;
+import com.lbe.sistemaponto.infra.security.DadosTokenJWT;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class AutenticacaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosAutenticacao> dadosJson;
+
+    @Autowired
+    private JacksonTester<DadosTokenJWT> dadosTokenJson;
+
+
+
+    @Test
+    @DisplayName("Teste de login - deveria devolver codigo http 200")
+    void loginCenario1() throws Exception {
+
+    }
+
+
+    @Test
+    @DisplayName("Teste de login - deveria devolver codigo http 400")
+    void loginCenario2() throws Exception {
+        var response = mvc.perform(post("/api/login")).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+
+}
---
+++ back/sistema-ponto/src/test/java/com/lbe/sistemaponto/controller/RegistroPontoControllerTest.java
@@ -0,0 +1,24 @@
+package com.lbe.sistemaponto.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistroPontoControllerTest {
+
+    @Test
+    void registrarBatidaPonto() {
+    }
+
+    @Test
+    void listarRegistroPorId() {
+    }
+
+    @Test
+    void listarRegistrosPorLogin() {
+    }
+
+    @Test
+    void ajustePonto() {
+    }
+}

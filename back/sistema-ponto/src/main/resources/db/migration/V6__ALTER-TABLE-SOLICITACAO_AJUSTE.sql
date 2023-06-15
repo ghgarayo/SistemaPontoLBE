416ee655520e5f476346c6a37e911ab4391e46fb
Gustavo Garayo <ghgarayo@gmail.com>
2023-06-15 18:33:30 -0300

    scripts antigos.

--- back/sistema-ponto/src/main/resources/db/migration/V6__ALTER-TABLE-SOLICITACAO_AJUSTE.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE solicitacao_ajuste MODIFY COLUMN Atualizado_Em_Hora time NULL;
-ALTER TABLE solicitacao_ajuste MODIFY COLUMN Atualizado_Em_Dia date NULL;
-ALTER TABLE solicitacao_ajuste MODIFY COLUMN Aprovado_Por bigint NULL;
